f150b1b435594301d676835b96c1a7b1cb3a53eb
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-07-14 23:10:57 -0700

    Changed a bit more

--- src/getting_started/Test.asm
+++ src/getting_started/Test.asm
@@ -1,34 +1,28 @@
- ; SNES Initialization Tutorial code
- ; This code is in the public domain.
- 
- .include "Header.inc"
- .include "Snes_Init.asm"
- 
- ; Needed to satisfy interrupt definition in "Header.inc".
- VBlank:
-   RTI
- 
- .bank 0
- .section "MainCode"
- 
- Start:
-     ; Initialize the SNES.
-     Snes_Init
- 
-     ; Set the background color to green.
-     sep     #$20        ; Set the A register to 8-bit.
-     lda     #%10000000  ; Force VBlank by turning off the screen.
-     sta     $2100
-     lda     #$F8        ; Load the low byte of the orange color.
-     sta     $2122
-     lda     #$00        ; Load the high byte of the orange color.
-     sta     $2122
-     lda     #%00001111  ; End VBlank, setting brightness to 15 (100%).
-     sta     $2100
- 
-     ; Loop forever.
- Forever:
-     jmp Forever
- 
- .ends
+;== include memorymap, header info, and SNES initialization routines
+.INCLUDE "Header.inc"
+.INCLUDE "InitSNES.asm"
+
+;========================
+; Start
+;========================
+
+.BANK 0 SLOT 0
+.ORG 0
+.SECTION "MainCode"
+
+Start:
+        InitSNES            ; Init Snes :)
+
+        stz $2121           ; Edit color 0 - snes' screen color you can write it in binary or hex
+        lda #%00011111      ; 
+        sta $2122
+        stz $2122           ; second byte has no data, so we write a 0
+
+        lda #$0F            ; = 00001111
+        sta $2100           ; Turn on screen, full brightness
+
+forever:
+        jmp forever
+
+.ENDS
 
